f9eb8e52ce99207ddec28aa55e34bcd7fae84f96
mistyk786 <47024210+mistyk786@users.noreply.github.com>
2026-07-26 11:10:17 +0800

    Time: 31 ms (20.77%) | Memory: 18.3 MB (9.41%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,18 +4,11 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        hm = {}
-        for str in strs:
-            str_lst = [0] * 26
-            for char in str:
-                pos = ord(char) - ord('a')
-                str_lst[pos] += 1
-            str_lst = tuple(str_lst)
-            lst = hm.get(str_lst, [])
-            lst.append(str)
-            hm[str_lst] = lst
+        groups = defaultdict(list)
+        for word in strs:
+            counts = [0] * 26
+            for char in word:
+                counts[ord(char) - ord("a")] += 1
+            groups[tuple(counts)].append(word)
 
-        res = []
-        for val in hm.values():
-            res.append(val)
-        return res
+        return list(groups.values())
